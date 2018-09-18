aef8e17100bb8ed9c0613da409c1ca7cb117877e
zhoushanlin3030 <zhoushanlin3@163.com>
2018-09-18 16:56:36 +0800

    sd

---
+++ src/com/kye/ConfigEntity.java
@@ -0,0 +1,15 @@
+package com.kye;
+/**
+ * 配置文件信息类
+ * @author Administrator
+ *
+ */
+public class ConfigEntity {
+	
+    public String updataFilePath = "";	
+    public String patchFilesPath = "";
+    public String bsdiff = "";
+    public String bspatch = "";
+    public String deviceName = "";
+
+}
---
+++ src/com/kye/UpdateJsonBean.java
@@ -0,0 +1,18 @@
+package com.kye;
+
+import java.util.Collections;
+import java.util.List;
+
+public class UpdateJsonBean {
+	public List<UpdataEntity> entities;
+    public String uploadVersion = "";
+    public String md5Str = "";
+    
+    public void writeDownPath() {
+    	Collections.sort(entities);
+    	Collections.reverse(entities);
+        for (int i = 0; i < entities.size(); i++) {
+            entities.get(i).downloadPath = String.format("patch%d.apk", i+1);
+        }
+    }
+}
